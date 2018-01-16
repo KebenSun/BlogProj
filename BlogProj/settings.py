474ee0f06a4f1bbd0b939ef36ac3d4c81d200016
sunkc <sun.kechong@outlook.com>
2018-01-16 16:26:45 +0800

    'prepare'

--- BlogProj/settings.py
+++ BlogProj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_c31)67)3^j*m2megnxj^ngwtukrl%l++f6b0jzxs(=wqc*3zg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.sshome.cc']
 
 
 # Application definition
@@ -128,3 +128,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
