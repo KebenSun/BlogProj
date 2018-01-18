b0e052866673d80a5707182da70c7dfabe799400
sunkc <sun.kechong@outlook.com>
2018-01-18 16:50:45 +0800

    close debug

--- BlogProj/settings.py
+++ BlogProj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_c31)67)3^j*m2megnxj^ngwtukrl%l++f6b0jzxs(=wqc*3zg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.sshome.cc']
 
